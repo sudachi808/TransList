9c52f6b3277619b0fe1d524c0695321872dfcd35
sudachi808 <sudachi808@gmail.com>
2019-09-02 22:07:39 +0900

    ルーツバーから戻れるように

--- app/src/main/java/com/example/translist/DetailActivity.kt
+++ app/src/main/java/com/example/translist/DetailActivity.kt
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.transition.*
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
@@ -28,6 +29,9 @@ class DetailActivity : AppCompatActivity() {
 
         setContentView(R.layout.detail_activity)
 
+        this.setSupportActionBar(this.toolbar)
+        this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         this.item?.let {
             println("_/_/ ${it.title}")
             this.titleTextView.text = it.title
@@ -36,6 +40,18 @@ class DetailActivity : AppCompatActivity() {
 
         findViewById<View>(android.R.id.content).systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        println("_/_/ $item")
+        when (item.itemId) {
+            android.R.id.home -> {
+                finishAfterTransition()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     inner class DetailTransitionSet : TransitionSet() {
